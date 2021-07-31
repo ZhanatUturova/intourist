4f5f76b656a7552c147332f6d69a167b282022ac
ZhanatUturova <63417019+ZhanatUturova@users.noreply.github.com>
2021-07-31 06:39:30 +0600

    add user foreighnKey in place model

--- places/models.py
+++ places/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Place(models.Model):
+    user = models.ForeignKey(   # создаем связь - много мест может разместить один пользователь
+        to=User, on_delete=models.SET_NULL,
+        null=True, blank=True
+    )  # от многого к одному
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True) # null - не обязательное поле для базы данных, blank - необязательно для админки
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
     views_count = models.IntegerField(default=0)
