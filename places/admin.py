d9a0745948963a98f32e13fe1fee110a67da55e2
ZhanatUturova <63417019+ZhanatUturova@users.noreply.github.com>
2021-08-07 04:31:28 +0600

    added admin

--- places/admin.py
+++ places/admin.py
@@ -3,4 +3,19 @@ from .models import Place, Feedback
 
 # Register your models here.
 admin.site.register(Place)
-admin.site.register(Feedback)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    # работа над списком элементов
+    list_display = ['text', 'place', 'user', 'checked']
+    list_editable = ['checked']
+
+    list_filter = ['checked']       # фильтрация
+
+    search_fields = ['text', 'place__name', 'place__location', 'place__description']        # поиск
+
+
+    # работа над одним элементом
+    fields = ['user', 'place', 'text']
+    readonly_fields = ['place', 'text']
+
+admin.site.register(Feedback, FeedbackAdmin)
--- places/models.py
+++ places/models.py
@@ -41,6 +41,8 @@ class Feedback(models.Model):
 
     text = models.TextField(verbose_name='Текст обратной связи')
 
+    checked = models.BooleanField(default=False, verbose_name='Обработано')
+
     def __str__(self) -> str:
         return self.text[:20]
 
