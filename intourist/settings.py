8c6d970cd5776f9bb1f3f4550761239ca6837997
ZhanatUturova <63417019+ZhanatUturova@users.noreply.github.com>
2021-08-10 04:37:03 +0600

    changed db

--- intourist/settings.py
+++ intourist/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'intourist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'inrourist_db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',        # or 'localhost'
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
